ec84de4998186bed924587713ce1df1adfff6306
mobeen-at-garn <mobeen.ikhtiar@i-workspace.online>
2025-08-15 18:13:25 +0500

    Tes

--- app/buyer/acquireprofile/page.tsx
+++ app/buyer/acquireprofile/page.tsx
@@ -10,7 +10,7 @@ import { Checkbox } from "@/components/ui/checkbox";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
-import { Toaster } from "@/components/ui/toaster";
+// import { Toaster } from "@/components/ui/toaster";
 import { Button } from "@/components/ui/button";
 import {
   Select,
